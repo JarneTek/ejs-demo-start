693f1ff519cb266c3c77937c9b50ddcd2ee5761d
«JarneTek» <jarnteki@student.arteveldehs.be>
2025-02-18 10:56:18 +0100

    set-up part 2

--- src/app.js
+++ src/app.js
@@ -2,23 +2,24 @@
 import express from "express";
 import path from "path";
 
+import { home, about, contact, privacy } from "./controllers/PageController.js";
+
 // create an instance of express
 const app = express();
 
-app.set("view engine", "ejs")
-app.set("views", path.resolve("src", "views"))
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
 
 // serve static files from the public folder
 // they can be accessed from the root of the site (e.g. /assets/images/dino_07.png) 🦕
 app.use(express.static("public"));
 
-// GET route to serve the index.html file
-app.get("/", (req, res) => {
-  res.render("home", {
-    title: "Dinosaurs are awesome",
-    content: "Dinosaurs and a lot of yap"
-  })
-});
+// page routes
+app.get("/", home);
+app.get("/about", about);
+app.get("/contact", contact);
+app.get("/privacy", privacy);
 
 // start the server, listen on port defined in .env file
 app.listen(process.env.PORT, () => {
